a4dfe9a798d6223ff7ac1a9afb6c268468f6adc3
Jon Seager <jon@sgrs.uk>
2025-04-04 14:48:40 +0100

    feat: add exception handling and logging

--- electron/patch-desktop-filename.py
+++ electron/patch-desktop-filename.py
@@ -34,29 +34,52 @@ import shutil
 import json
 import sys
 import os
+import logging
 from pathlib import Path
 
+logger = logging.getLogger(__name__)
 
 def main() -> None:
     asar_path = sys.argv[1]
     extract_dir = Path(tempfile.mktemp())
     package_json_path = extract_dir / "package.json"
 
-    asarPy.extract_asar(asar_path, extract_dir)
+    logger.info("Attempting to patch desktop file name for asar file: {}", asar_path)
 
-    with open(package_json_path, "r", encoding="utf-8") as f:
-        package_json = json.load(f)
+    try:
+        asarPy.extract_asar(asar_path, extract_dir)
+    except Exception as e:
+        logger.error(f"Failed to extract asar file: {e}")
+        exit(1)
+
+    try:
+        with open(package_json_path, "r", encoding="utf-8") as f:
+            package_json = json.load(f)
+    except Exception as e:
+        logger.error(f"Failed to load package.json: {e}")
+        exit(1)
 
     project_name = os.getenv("CRAFT_PROJECT_NAME")
     if project_name is None:
+        logger.error("CRAFT_PROJECT_NAME environment variable is not set, exiting")
         exit(1)
 
     package_json["desktopName"] = f"{project_name}_{project_name}.desktop"
 
-    with open(package_json_path, "w", encoding="utf-8") as f:
-        json.dump(package_json, f)
+    try:
+        with open(package_json_path, "w", encoding="utf-8") as f:
+            json.dump(package_json, f)
+    except Exception as e:
+        logger.error(f"Failed to write package.json: {e}")
+        exit(1)
 
-    asarPy.pack_asar(extract_dir, asar_path)
+    try:
+        asarPy.pack_asar(extract_dir, asar_path)
+    except Exception as e:
+        logger.error(f"Failed to re-pack asar file: {e}")
+        exit(1)
+
+    logger.info("Asar file patched successfully")
 
     shutil.rmtree(extract_dir)
 
